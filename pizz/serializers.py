eec76d38cbe03cc4458fe4a4f57e964ed1481078
Rafael Galiev <rafael.galiev.kazan@gmail.com>
2018-07-03 23:08:58 +0300

    Serializer experiment

--- pizz/serializers.py
+++ pizz/serializers.py
@@ -17,11 +17,13 @@ class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         """Meta class to map serializer's fields with the Customer.model fields."""
         model = Customer
+        # fields = ('name', 'adress',)
         fields = '__all__'
 
 
 class OrderSerializer(serializers.ModelSerializer):
     """Serializer to map the Order.Model instance into JSON format"""
+    # customer = CustomerSerializer(required=True)
 
     class Meta:
         """Meta class to map serializer's fields with the Order.model fields."""
